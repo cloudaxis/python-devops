7ac558ad1d0337ebbdb3d7131d87c961b390680e
Michael Welsh <michael.welsh@cloudaxis.com>
2022-07-01 14:11:22 +0800

    added assertion for meal

--- test_devopslib.py
+++ test_devopslib.py
@@ -3,3 +3,7 @@ from devopslib.randomfruit import fruit
 def test_fruit():
     fruit_choice = fruit()
     assert fruit_choice in ["apple", "cherry", "strawberry"]
+
+def test_meal():
+    result = meal("beer")
+    assert "beer" in result
